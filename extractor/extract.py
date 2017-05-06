ba076dd60c780ee8882d435b646dcd578fcb3ee8
Alaa El Bouchti <alaa_23@hotmail.fr>
2017-05-06 23:25:34 +0200

    Update extract.py

--- extractor/extract.py
+++ extractor/extract.py
@@ -2,17 +2,11 @@ listspecial = ';[](),{=}'
 listword = []
 
 
-
+@staticmethod
 def extract_from(file, list_word):
-    with open(file, 'r', encoding = 'utf-8', errors= 'ignore') as f:
-        for lines in f.read():
-            if len(lines) == 0 or str(lines)[0] != '*':
-                continue
-            word = ''
-            for i in lines:
-                if str(i) in list_special:
-                    continue
-                    while (not str(i) in list_special):
-                        word += str(i)
-                        print (word)
-                        list_word.append(word)
+    with open(file) as f:
+        for i in f.readlines():
+            if str(i)[0] == '*':
+                print (i)
+            
+        
