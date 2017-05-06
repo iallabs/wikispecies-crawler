e61b84e434aea0d8c319281c5a418f201846d8e3
Alaa El Bouchti <alaa_23@hotmail.fr>
2017-05-06 23:43:22 +0200

    Update extract.py

--- extractor/extract.py
+++ extractor/extract.py
@@ -9,5 +9,20 @@ def extract_important(file, list_word):
         for i in f.readlines():
             if str(i)[0] == '*':
                 print (i)
-            
-        
+
+                
+def filter(listword):
+    for i in listword:
+        h = []
+        a = ''
+        if str(i[2]) == '{':
+            break
+        for char in i:
+            if str(char) == '*' or str(char) == '[':
+                continue
+            else:
+                if str(char) == ']':
+                    break
+                a += str(char)
+        print (a)
+        h.append(a)
