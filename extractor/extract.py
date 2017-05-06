15f77187b98306074cc5ddfe56f4f6f17b8e352b
Alaa El Bouchti <alaa_23@hotmail.fr>
2017-05-06 22:38:52 +0200

    Update extract.py

--- extractor/extract.py
+++ extractor/extract.py
@@ -1,1 +1,21 @@
-
+class extract():
+    def __init__(self):
+        self.wordlist = []
+        self.listspecial = ';[](),={}'
+        
+        def extract_from(file):
+            with open('C:\data.txt', 'r', encoding = 'utf-8', errors= 'ignore') as f:
+                for lines in f:
+                    if len(lines) == 0:
+                        continue
+                    for i in lines:
+                        if str(i) != '*':
+                            
+                        else:
+                            word = ''
+                            if str(i) in list_special:
+                                continue
+                            while str(i) not in list_special:
+                                word += str(i)
+                            print (word)
+                            list_word.append(word)
