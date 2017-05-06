82882f1399c9475b4c0e23c4647a5305233cf912
Hilaly <hilalyamine@gmail.com>
2017-05-06 23:27:06 +0200

    Update extract.py

--- extractor/extract.py
+++ extractor/extract.py
@@ -3,8 +3,9 @@ listword = []
 
 
 @staticmethod
-def extract_from(file, list_word):
+def extract_important(file, list_word):
     with open(file) as f:
+        # d = f.readlines()
         for i in f.readlines():
             if str(i)[0] == '*':
                 print (i)
