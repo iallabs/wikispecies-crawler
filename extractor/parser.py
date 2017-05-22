6806270362715ebf266b915aacf729c197854e58
Hilaly <hilalyamine@gmail.com>
2017-05-22 09:03:41 +0200

    Update parser.py

--- extractor/parser.py
+++ extractor/parser.py
@@ -11,11 +11,31 @@ def curl_p(url):
 
 # Should store a Ref class containing all synonyms of taxon to use in next research
 
-def find_taxon_data(name):
+find_taxon_data(soup.find('h2').next_sibling.next_sibling.contents, 'Charophyta')
+
+def find_taxon_data(contents, name):
+    '''
     soup = bs4.BeautifulSoup(wlink+name)
     target = soup.find('h2')
     contents = [i.string for i in target.next_sibling.next_sibling.contents]
-    for i in contents:
-        if not name = i
-            continue
+    '''
+    taxon_found = False
+    nefew_taxon = False
+    data = []
+    for j in contents:
+        i = j.string
+        print(i)
+        if not taxon_found:
+            if not (name == i):
+                continue
+            else:
+                taxon_found = True
+        else:
+            if i in Taxon_Strings:
+                return data
+            else:
+                if i==' - ':
+                    continue
+                data+=[i]
+                
     
