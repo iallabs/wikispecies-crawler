4f1531fc972ec9830942e655af45f08f4fefcbfa
Hilaly <hilalyamine@gmail.com>
2017-05-22 09:40:26 +0200

    Update parser.py

--- extractor/parser.py
+++ extractor/parser.py
@@ -6,30 +6,41 @@ glink = 'https://species.wikimedia.org/'
 wlink = 'https://species.wikimedia.org/wiki/'
 
 Taxon_strings = ['SuperKingdom'
-                    'Kingdom',
-                    'SubKingdom',
-                    'InfraKingdom',
-                    'Regnum',
-                    'SubRegnum',
-                    'Phylum',
-                    'SubPhylum',
-                    'Class',
-                    'SubClass',
-                    'Order',
-                    'SubOrder',
-                    'Familly',
-                    'SubFamilly',
-                    'Tribe',
-                    'SubTribe',
-                    'Genus',
-                    'SubGenus',
-                    'Section',
-                    'SubSection',
-                    'Series',
-                    'SubSeries',
-                    'Species',
-                    'SubSpecies',
-                    'Variety']
+                 'Kingdom',
+                 'SubKingdom',
+                 'InfraKingdom',
+                 'Regnum',
+                 'SubRegnum',
+                 'Phylum',
+                 'SubPhylum',
+                 'Class',
+                 'SubClass',
+                 'Order',
+                 'SubOrder',
+                 'Familly',
+                 'SubFamilly',
+                 'Tribe',
+                 'SubTribe',
+                 'Genus',
+                 'SubGenus',
+                 'Section',
+                 'SubSection',
+                 'Series',
+                 'SubSeries',
+                 'Species',
+                 'SubSpecies',
+                 'Variety',
+                 'Divisiones',
+                 'Subdivisiones',
+                 'Ordines',
+                 'Familia',
+                 'Ordo',
+                 'Familiae',
+                 'Genera',
+                 'Classis',
+                 'Classes',
+                 'Subclasses',
+                 'Subclassis']
 
 def curl_p(url):
     return urllib.request.urlopen(url).readline().decode('utf-8')
@@ -47,21 +58,38 @@ def find_taxon_data(contents, name):
     taxon_found = False
     nefew_taxon = False
     data = []
+    c=0
     for j in contents:
+        print('--------', j)
+        print('--------', taxon_found, c)
         i = j.string
-        print(i)
+        if i == '<br/>' or j=='\n':
+            print('none')
+            continue
         if not taxon_found:
-            if not (name == i):
+            if i is None:
+                print('noooooonnnnneeeee')
+            elif not (name == i[0:len(i)-1]):
+                print('not name', i)
                 continue
             else:
                 taxon_found = True
+                print('taxon found', i)
         else:
-            if i in Taxon_strings:
-                return data
+            if not i:
+                print('brk')
+                continue
+            if i[0:len(i)-1] in Taxon_strings:
+                c+=1
+                print(i, 'in taxon strings')
+                if c==2:
+                    print('break')
+                    return data
             else:
-                if i==' - ':
+                if i==' - ' or j==' - ':
+                    print('-')
                     continue
+                print('added data', i)
                 data+=[i]
-    return data
                 
     
